acacd4434c7961ffab86a1946745a049b20406b0
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2022-08-07 23:01:58 -0600

    Scrape UofA table

--- scrapper.py
+++ scrapper.py
@@ -3,43 +3,34 @@ import requests
 import sqlite3
 import numpy as np
 
+has_conflict = False
+day_letters = ["M", "T", "W", "H", "F"]
+
+schedule_monday = []
+schedule_tuesday = []
+schedule_wednesday = []
+schedule_thursday = []
+schedule_friday = []
+schedule = [schedule_monday, schedule_tuesday, schedule_wednesday, schedule_thursday, schedule_friday]
+
+##########################################################################################################################
+
+courses_requested = [('cmput', '466'), ('cmput', '365'), ('cmput', '291'), ('cmput', '340'), ('cmput', '256'), ('cmput', '325'), ('cmput', '379'), ('cmput', '404')]
+
+##########################################################################################################################
+
+# mySql_insert_query = '''INSERT OR IGNORE INTO cmput_courses VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+
 con = sqlite3.connect('scheduleSimple.db')
 cur = con.cursor()
 
 # cur.execute('''DROP TABLE cmput_courses''')
 
 # cur.execute('''CREATE TABLE IF NOT EXISTS cmput_courses
-#                 (course_name text, course_number int, section_type text, section_name text, section_num int PRIMARY KEY, capacity int, start_date text, 
+#                 (course_name text, section_type text, section_name text, section_num int PRIMARY KEY, capacity int, start_date text, 
 #                 end_date text, week_days text, start_time text, end_time text, location text)''')
 
-
-course_name = 'cmput'
-course_number = '229'
-
-url = f"https://apps.ualberta.ca/catalogue/course/{course_name}/{course_number}"
-# url = "https://apps.ualberta.ca/catalogue/course/cmput/229"
-
-
-# result = requests.get(url)
-# doc = BeautifulSoup(result.text, 'lxml')
-# datesTimes = doc.find_all("td",  {"data-card-title":"Section"})
-
-# mySql_insert_query = '''INSERT OR IGNORE INTO cmput_courses VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
-
-# for date in datesTimes:
-
-#     text = "  ".join(date.parent.text.split())
-#     textArr = text.split()
-#     textArr[:] = (value for value in textArr if value != '-')
-
-#              #['LECTURE',      'A1',          '(36739)',        '75',   '2022-09-01', '2022-12-08', 'MWF',    '13:00',   '13:50',           '(CAB', '239)',                        'Rupam', 'Mahmood']
-#     #record = (textArr[0], textArr[1], textArr[2].strip("()"), textArr[3], textArr[4], textArr[5], textArr[6], textArr[7], textArr[8], str(textArr[9] + " " + textArr[10]).strip("()"), str(textArr[13] + " " + textArr[14]))
-#     record = (course_name.upper(), course_number, textArr[0], textArr[1], textArr[2].strip("()"), textArr[3], textArr[4], textArr[5], textArr[6], textArr[7], textArr[8], str(textArr[9] + " " + textArr[10]).strip("()"))
-#     cur.execute(mySql_insert_query, record)
-
-# con.commit()
-
-day_letters = ["M", "T", "W", "H", "F"]
+#=========================================================================================================================
 
 def convert_day(day):
     days = list(day)
@@ -48,16 +39,69 @@ def convert_day(day):
         return_days.append(day_letters.index(a_day))
     return return_days
 
+#=========================================================================================================================
 
-schedule = np.zeros((5, 14))
+# for course in courses_requested:
+#     url = f"https://apps.ualberta.ca/catalogue/course/{course[0]}/{course[1]}"
+#     result = requests.get(url)
+#     doc = BeautifulSoup(result.text, 'lxml')
+#     datesTimes = doc.find_all("td",  {"data-card-title":"Section"})
 
-for row in cur.execute('''SELECT * FROM cmput_courses WHERE section_type="LECTURE";'''):
-    start_hour = int((row[9].split(":")[0]).lstrip("0")) - 8
-    # print(row[0:2] + row[8:11])
-    print(start_hour)
-    print(row[8])
-    print(convert_day(row[8]))
-    for a_day in convert_day(row[8]):
-        schedule[a_day][start_hour] = 1
+#     for date in datesTimes:
 
-print(schedule)
+#         text = "  ".join(date.parent.text.split())
+#         textArr = text.split()
+#         textArr[:] = (value for value in textArr if value != '-')
+#         textArr[:] = (value for value in textArr if value != 'ONLINE')
+
+#         record = (f'{course[0].upper()} {course[1]}', textArr[0], textArr[1], textArr[2].strip("()"), textArr[3],
+#                     textArr[4], textArr[5], textArr[6], textArr[7], textArr[8], str(textArr[9] + " " + textArr[10]).strip("()"))
+#         cur.execute(mySql_insert_query, record)
+
+# con.commit()
+
+
+url = f"https://www.beartracks.ualberta.ca/psc/uahebprd_4/EMPLOYEE/HRMS/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, 'lxml')
+datesTimes = doc.find_all("td",  {"data-card-title":"Section"})
+
+#=========================================================================================================================
+
+course_names_selected = []
+
+for db_course in cur.execute('''SELECT * FROM cmput_courses WHERE section_type = "LECTURE" AND start_date = "2023-01-05"'''):
+    has_conflict = False
+
+    if db_course[0] in course_names_selected:
+        continue
+
+    for a_day in convert_day(db_course[7]):
+        for course in schedule[a_day]:
+
+            existing_start_time = int(str(course[1]).replace(":",""))
+            existing_end_time = int(str(course[2]).replace(":",""))
+
+            db_course_start_time = int(str(db_course[8]).replace(":",""))
+            db_course_end_time = int(str(db_course[9]).replace(":",""))
+
+            if db_course_start_time <= existing_end_time and db_course_end_time >= existing_start_time:
+                has_conflict = True
+
+    if not has_conflict:
+        for a_day in convert_day(db_course[7]):
+            schedule[a_day].append([db_course[0], db_course[8], db_course[9], db_course[3]])
+            course_names_selected.append(db_course[0])
+#=========================================================================================================================
+
+# print out the schedule at the end
+for i in range(len(schedule)):
+    print(day_letters[i])
+    for j in range(len(schedule[i])):
+        print(schedule[i][j])
+    print("")
+
+# for db_course in cur.execute('''SELECT * FROM cmput_courses WHERE section_type="LECTURE"'''):
+#     print(db_course)
+
+#=========================================================================================================================
