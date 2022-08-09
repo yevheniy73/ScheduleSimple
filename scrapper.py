ff57a299a20bdc35767689abc3fe57836ee2e7ae
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2022-08-08 20:43:21 -0600

    We are in time table business, showing correct schedule given the course and their times

--- scrapper.py
+++ scrapper.py
@@ -1,11 +1,14 @@
+from calendar import day_name
+from ntpath import join
+from operator import indexOf
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
 import numpy as np
 import os
-import re
 
 has_conflict = False
+course_colors = ["#ff9999", "#99FF99", "#ff99cc", "#FFFF99", "#99CCFF", "#CC99FF"]
 day_letters = ["M", "T", "W", "H", "F"]
 
 schedule_monday = []
@@ -21,16 +24,16 @@ courses_requested = [('cmput', '466'), ('cmput', '365'), ('cmput', '291'), ('cmp
 
 ##########################################################################################################################
 
-# mySql_insert_query = '''INSERT OR IGNORE INTO cmput_courses VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+mySql_insert_query = '''INSERT OR IGNORE INTO cmput_courses VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
 
 con = sqlite3.connect('scheduleSimple.db')
 cur = con.cursor()
 
 # cur.execute('''DROP TABLE cmput_courses''')
 
-# cur.execute('''CREATE TABLE IF NOT EXISTS cmput_courses
-#                 (course_name text, section_type text, section_name text, section_num int PRIMARY KEY, capacity int, start_date text, 
-#                 end_date text, week_days text, start_time text, end_time text, location text)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS cmput_courses
+                (course_name text, section_type text, section_name text, section_num int PRIMARY KEY, capacity int, start_date text, 
+                end_date text, week_days text, start_time text, end_time text, location text)''')
 
 #=========================================================================================================================
 
@@ -87,49 +90,74 @@ for db_course in cur.execute('''SELECT * FROM cmput_courses WHERE section_type =
     if not has_conflict:
         for a_day in convert_day(db_course[7]):
             schedule[a_day].append([db_course[0], db_course[8], db_course[9], db_course[3]])
-            course_names_selected.append(db_course[0])
+        course_names_selected.append(db_course[0])
+
 #=========================================================================================================================
 
 # print out the schedule at the end
-# for i in range(len(schedule)):
-#     print(day_letters[i])
-#     for j in range(len(schedule[i])):
-#         print(schedule[i][j])
-#     print("")
+for i in range(len(schedule)):
+    print(day_letters[i])
+    for j in range(len(schedule[i])):
+        print(schedule[i][j])
+    print("")
 
 # for db_course in cur.execute('''SELECT * FROM cmput_courses WHERE section_type="LECTURE"'''):
 #     print(db_course)
 
 #=========================================================================================================================
 
-print(schedule[0])
 
 base = os.path.dirname(os.path.abspath(__file__))
-
 
 html_sched =open(os.path.join(base, 'SampleTemplate.html'))
 html_sched_text = BeautifulSoup(html_sched, 'lxml')
 
 
-for class_section in schedule[0]:
+day_name_pointer = 0
 
-    var1 = class_section[1].replace(":","")
-    sched_block = html_sched_text.find("td", {"id": f"M{var1}a"})
+for day in schedule:
+    day_name_pointer += 1
+    day_name = day_letters[day_name_pointer - 1]
 
-    sched_block.string = "CMPUT 325 <br> LEC B1 (44762) <br> CCIS 1-140"
-    sched_block['class'] = "ZSSCLSSCHEDSTDCLS"
-    sched_block['height'] = "60"
-    sched_block['width'] = "100"
-    sched_block['rowspan'] = "6"
-    sched_block['colspan'] = "2"
-    sched_block['bgcolor'] = "#FFFF99"
+    for course in day:
 
-    sched_block = html_sched_text.find("td",  {"id":"M1115a"})
-    sched_block['class'] = sched_block.get('class', []) + ['HIDE_ME']
+        course_start_time = course[1].replace(":","")
+        course_end_time = course[2].replace(":","")
 
+        time_difference_hours = int(course_end_time[:2]) - int(course_start_time[:2])
+        time_difference_mins = int(course_end_time[-2:]) - int(course_start_time[-2:]) + 10
+        time_difference = time_difference_hours * 60 + time_difference_mins
 
+        sched_block = html_sched_text.find("td", {"id": f"{day_name}{course_start_time}a"})
+        sched_block_b = html_sched_text.find("td", {"id": f"{day_name}{course_start_time}b"})
+        sched_block_b['class'] = sched_block.get('class', []) + ['HIDE_ME']
 
+        sched_block.string = f"{course[0]} LEC {course[3]}"
+        sched_block['class'] = "ZSSCLSSCHEDSTDCLS"
+        sched_block['height'] = time_difference
+        sched_block['width'] = "100"
+        sched_block['rowspan'] = (time_difference / 15)
+        sched_block['colspan'] = "2"
+        sched_block['bgcolor'] = course_colors[course_names_selected.index(course[0])]
 
+        sched_pointer = 15
+
+        while sched_pointer< time_difference:
+
+            sched_hour = course_start_time[:2]
+            sched_minute = int(course_start_time[-2:]) + sched_pointer
+            extra_hour, extra_minute = divmod(sched_minute, 60)
+
+            sched_hour = str(int(course_start_time[:2]) + extra_hour).zfill(2)
+            extra_minute = str(extra_minute).zfill(2)
+
+            sched_block = html_sched_text.find("td", {"id": f"{day_name}{sched_hour}{extra_minute}a"})
+            sched_block['class'] = sched_block.get('class', []) + ['HIDE_ME']
+
+            sched_block = html_sched_text.find("td", {"id": f"{day_name}{str(sched_hour).zfill(2)}{str(extra_minute).zfill(2)}b"})
+            sched_block['class'] = sched_block.get('class', []) + ['HIDE_ME']
+
+            sched_pointer += 15
 
 with open("output1.html", "w") as file:
     file.write(str(html_sched_text.prettify()))
